0353d9563da056559d2431e872774cbdb5011eb8
DarcyHe <suan24@qq.com>
2018-12-24 16:48:08 +0800

    完成歌手页面组件，新增部分player内容

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,14 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <player></player>
   </div>
 </template>
 
 <script>
   import mHeader from '@/components/m-header/m-header'
   import tab from '@/components/tab/tab'
+  import player from '@/components/player/player'
   
   
   export default {
@@ -21,7 +23,8 @@
     },
     components: {
       mHeader,
-      tab
+      tab,
+      player
     },
   }
 </script>
